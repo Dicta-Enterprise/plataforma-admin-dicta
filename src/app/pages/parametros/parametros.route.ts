07546a3753a3831e9bb09e1bc9e3daa507a35ed0
Jorge84307Gaitan <jgaitan84307@universidadean.edu.co>
2026-01-19 14:59:35 -0500

    se implemento la pantalla de parametros junto con su modal correspondiente

--- src/app/pages/parametros/parametros.route.ts
+++ src/app/pages/parametros/parametros.route.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Parametros } from './parametros';
 
+
 export const PARAMETROS_ROUTES: Routes = [
   {
     path: '',
--- src/app/pages/parametros/parametros.ts
+++ src/app/pages/parametros/parametros.ts
@@ -1,39 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TreeTableModule } from 'primeng/treetable';
+import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
-import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms';
 
-import { TreeNode } from 'primeng/api';
+interface ParameterData {
+  name: string;
+  size: string;
+  type: string;
+}
+
+interface Parameter {
+  data: ParameterData;
+  children?: Parameter[];
+}
 
 @Component({
   selector: 'app-parametros',
   standalone: true,
-  imports: [
-    CommonModule,
-    TreeTableModule,
-    ButtonModule,
-    DialogModule,
-    InputTextModule,
-    FormsModule
-  ],
+  imports: [CommonModule, ButtonModule, AccordionModule, TableModule, DialogModule],
   templateUrl: './parametros.html',
-  styleUrls: ['./parametros.css']
+  styleUrls: ['./parametros.css'],
 })
 export class Parametros {
-  
-  files: TreeNode[] = [
+  parameters: Parameter[] = [
     {
-      data: { name: 'Categorías', size: '', type: 'Sección' },
+      data: { name: 'Categorías', size: '', type: 'SecciónCategoria' },
       children: [
         { data: { name: 'Categoría 1', size: '1', type: 'CAT1' } },
         { data: { name: 'Categoría 2', size: '2', type: 'CAT2' } }
       ]
     },
     {
-      data: { name: 'Documentos de Identidad', size: '', type: 'Sección' },
+      data: { name: 'Documentos de Identidad', size: '', type: 'SecciónDocumentoIdentidad' },
       children: [
         { data: { name: 'Documento Nacional de Identidad', size: '1', type: 'DNI' } },
         { data: { name: 'Pasaporte', size: '2', type: 'PAS' } }
@@ -41,32 +41,9 @@ export class Parametros {
     }
   ];
 
-  displayModal = false;
-  newValue = '';
-  newCode = '';
-  currentNode: TreeNode | null = null;
+  visible = false;
 
-  openNewValueModal(node: TreeNode) {
-    this.currentNode = node;
-    this.newValue = '';
-    this.newCode = '';
-    this.displayModal = true;
-  }
-
-  saveNewValue() {
-    if (this.currentNode) {
-      if (!this.currentNode.children) {
-        this.currentNode.children = [];
-      }
-      const newId = this.currentNode.children.length + 1;
-      this.currentNode.children.push({
-        data: {
-          name: this.newValue,
-          size: newId.toString(),
-          type: this.newCode
-        }
-      });
-      this.displayModal = false;
-    }
+  showDialog() {
+    this.visible = true;
   }
 }
